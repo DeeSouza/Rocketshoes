92aed94915b0a7e99a3ec916cb2b94e77e552a57
Diego Souza <dee-souza@hotmail.com>
2019-08-27 16:58:04 -0300

    Feat: Style home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,113 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-	return <div>das</div>;
+	return (
+		<ProductList>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/58/HZM-1880-058/HZM-1880-058_detalhe1.jpg?ims=326x"
+					alt="Tênis"
+				/>
+
+				<strong>Chuteira Top</strong>
+				<span>R$ 129,90</span>
+
+				<button type="button">
+					<div>
+						<MdAddShoppingCart size={16} color="#FFF" /> 3
+					</div>
+
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+		</ProductList>
+	);
 }
