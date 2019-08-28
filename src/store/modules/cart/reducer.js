b348c2b9b25136232dacaf278f0cf5e6c1e0689e
Diego Souza <dee-souza@hotmail.com>
2019-08-28 13:50:01 -0300

    Feat: remove product from cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -7,6 +7,7 @@ export default function cart(state = [], action) {
 				const productIndex = draft.findIndex(
 					p => p.id === action.product.id
 				);
+
 				if (productIndex >= 0) {
 					draft[productIndex].amount += 1;
 				} else {
@@ -14,6 +15,14 @@ export default function cart(state = [], action) {
 						...action.product,
 						amount: 1,
 					});
+				}
+			});
+		case 'REMOVE_FROM_CART':
+			return produce(state, draft => {
+				const productIndex = draft.findIndex(p => p.id === action.id);
+
+				if (productIndex >= 0) {
+					draft.splice(productIndex, 1);
 				}
 			});
 		default:
