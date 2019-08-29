0b3225113eaad057ac521e236360624b4d6d7702
Diego Souza <dee-souza@hotmail.com>
2019-08-29 16:16:28 -0300

    Feat: decrement and increment product, check stock

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -14,13 +14,13 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount, total }) {
+function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
 	function increment(product) {
-		updateAmount(product.id, product.amount + 1);
+		updateAmountRequest(product.id, product.amount + 1);
 	}
 
 	function decrement(product) {
-		updateAmount(product.id, product.amount - 1);
+		updateAmountRequest(product.id, product.amount - 1);
 	}
 
 	return (
@@ -134,6 +134,6 @@ Cart.propTypes = {
 		})
 	).isRequired,
 	removeFromCart: PropTypes.func.isRequired,
-	updateAmount: PropTypes.func.isRequired,
+	updateAmountRequest: PropTypes.func.isRequired,
 	total: PropTypes.string.isRequired,
 };
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,7 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 /**
  * Funcionalidade do JS (Generator)
@@ -28,7 +28,7 @@ function* addToCart({ id }) {
 	}
 
 	if (productExists) {
-		yield put(updateAmount(id, amount));
+		yield put(updateAmountSuccess(id, amount));
 	} else {
 		// Como se fosse o await
 		const response = yield call(api.get, `/products/${id}`);
@@ -44,7 +44,24 @@ function* addToCart({ id }) {
 	}
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+	if (amount <= 0) return;
+
+	const stock = yield call(api.get, `stock/${id}`);
+	const stockAmount = stock.data.amount;
+
+	if (amount > stockAmount) {
+		toast.error('Quantidade solicitada fora de estoque!');
+		return;
+	}
+
+	yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+	takeLatest('@cart/ADD_REQUEST', addToCart),
+	takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
 
 // TakeLatest usa a última chamada
 // TakeEvery chama todas as vezes
